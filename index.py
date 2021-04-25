3ec408d144b2403ab2d3367a40dc89d7c1cb5c0c
nhporti2 <nhporti2@gmail.com>
2021-04-24 20:18:54 -0500

    Update index.py

--- index.py
+++ index.py
@@ -13,16 +13,20 @@ print(new_places)
 
 #Output: ['Victor aNisimov', 'Gary A.J. Bernstein', 'Joel Carter', 'Andrew P. Garfield', 'David hassELHOFF']
 
+author = ["Joel Carter", "Victor aNisimov", "Andrew P. Garfield","David hassELHOFF","Gary A.J. Bernstein"]
 
-
-
-
-
-
+author.sort(key=lambda x:x.split(" ")[-1].lower())
+print(author)
 
 #Convert the list below from Celsius to Farhenheit, using the map function with a lambda...
 
 #Output: [('Nashua', 89.6), ('Boston', 53.6), ('Los Angelos', 111.2), ('Miami', 84.2)]
+
+places = [('Nashua',32),("Boston",12),("Los Angelos",44),("Miami",29)]   
+
+final_places=list(map(lambda x: (x[0], (9/5)*x[1] + 32), places))
+ 
+print(final_places)
 
 #Write a recursion function to perform the fibonacci sequence up to the number passed in.
 
@@ -32,7 +36,7 @@ def fibonacci(n):
 	else:
 		return(fibonacci(n-1) + fibonacci(n-2))
 
-n = int(input('Enter a number, N, N>=2 : '))
+n = int(input('Enter a number, N, N >= 2 : '))
 
 fibo_series = []
 
